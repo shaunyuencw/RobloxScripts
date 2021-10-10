19358675047909b800476e2de91dda4783624746
Shaun Yuen <91139027+shaunyuencw@users.noreply.github.com>
2021-10-10 18:24:44 +0800

    Update and rename NeutralSelectorScript.lua to TeamSelectorScript.lua

--- Dodgeball/TeamSelectorScript.lua
+++ Dodgeball/TeamSelectorScript.lua
@@ -5,11 +5,12 @@ function selectTeam(part)
 	
 	--if player exists change team
 	if player then
-		--change the team
+		--change the team to whichever Team Name you have
 		player.Team = game:GetService("Teams"):FindFirstChild("NeutralTeam")
-		--teleport the player, make sure you have a Model called NeutralSpawn with a SpawnLocation in it.
+		--teleport the player, make sure you have a Model with a SpawnLocation in it.
 		character:FindFirstChild("HumanoidRootPart").CFrame = game.Workspace.NeutralSpawn.SpawnLocation.CFrame + Vector3.new(0, 5, 0)
 		
+		-- Repeat for however many teams you have!
 	end
 	
 end
